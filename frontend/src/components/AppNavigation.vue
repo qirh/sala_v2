15261ffb428e20c11877d5026c87d7d6a54746cd
qirh <saleh@alghusson.com>
2019-02-10 20:46:27 -0500

    md navigation

--- frontend/src/components/AppNavigation.vue
+++ frontend/src/components/AppNavigation.vue
@@ -1,29 +1,20 @@
 <template>
-    <v-toolbar app color="brown darken-4" dark>
-        <v-toolbar-side-icon
-            class="hidden-md-and-up"
-            @click="drawer = !drawer"
-        ></v-toolbar-side-icon>
-        <v-spacer class="hidden-md-and-up"></v-spacer>
-        <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
-        <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
-        <v-spacer class="hidden-sm-and-down"></v-spacer>
-        <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
-        <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+    <v-toolbar app color="grey lighten-5">
+        <v-toolbar-items>
+            <v-btn flat>/home</v-btn>
+            <v-btn flat>/blog</v-btn>
+        </v-toolbar-items>
     </v-toolbar>
 </template>
 
 <script>
 export default {
     name: 'AppNavigation',
-    data() {
-        return {
-            appTitle: 'Meal Prep',
-            drawer: false,
-            items: [{title: 'Menu'}, {title: 'Sign In'}, {title: 'Join'}],
-        };
-    },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+* {
+    text-transform: none !important;
+}
+</style>

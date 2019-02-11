f622e6d4f85201bf607d2927cff2989970f2d848
qirh <saleh@alghusson.com>
2019-02-10 20:26:42 -0500

    add navigation bar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,22 @@
 <template>
     <v-app>
-        <v-content transition='slide-x-transition'>
+        <app-navigation></app-navigation>
+
+        <v-content transition="slide-x-transition">
             <router-view></router-view>
         </v-content>
     </v-app>
 </template>
 
 <script>
+import AppNavigation from '@/components/AppNavigation';
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        AppNavigation,
+    },
 };
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ frontend/src/components/AppNavigation.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-toolbar app color="brown darken-4" dark>
+        <v-toolbar-side-icon
+            class="hidden-md-and-up"
+            @click="drawer = !drawer"
+        ></v-toolbar-side-icon>
+        <v-spacer class="hidden-md-and-up"></v-spacer>
+        <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+        <v-btn flat class="hidden-sm-and-down">Menu</v-btn>
+        <v-spacer class="hidden-sm-and-down"></v-spacer>
+        <v-btn flat class="hidden-sm-and-down">SIGN IN</v-btn>
+        <v-btn color="brown lighten-3" class="hidden-sm-and-down">JOIN</v-btn>
+    </v-toolbar>
+</template>
+
+<script>
+export default {
+    name: 'AppNavigation',
+    data() {
+        return {
+            appTitle: 'Meal Prep',
+            drawer: false,
+            items: [{title: 'Menu'}, {title: 'Sign In'}, {title: 'Join'}],
+        };
+    },
+};
+</script>
+
+<style scoped></style>

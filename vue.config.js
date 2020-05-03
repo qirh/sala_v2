9f0d2af1548a968869e31ab7dada66fe9083befb
saleh <saleh@alghusson.com>
2020-05-03 19:51:07 -0400

    vue config?

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
+      locale: 'ar',
+      fallbackLocale: 'ar',
       localeDir: 'locales',
       enableInSFC: true
     }

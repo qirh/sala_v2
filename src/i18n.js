f3a2f64fa9f59f5e136c11a47dba69cc1274233d
saleh <saleh@alghusson.com>
2020-05-03 22:46:22 -0400

    en

--- src/i18n.js
+++ src/i18n.js
@@ -3,7 +3,7 @@ import VueI18n from 'vue-i18n';
 
 Vue.use(VueI18n);
 
-const defaultLocale = 'es';
+const defaultLocale = 'en';
 function loadLocaleMessages() {
     const locales = require.context(
         './locales',

63523f2c76f47363b3eefc5723fc36d1202b60bd
saleh <saleh@alghusson.com>
2020-05-05 02:41:01 -0400

    fixed langs

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -1,18 +1,16 @@
 <template>
     <div v-if="langs.length" class="langs-list">
-        <div v-for="lang in langs" :key="lang.code">
-            <div
-                v-on:click="changeLang(lang.code)"
-                :title="lang.code"
-                :disabled="$store.state.currentLang === lang.code"
-                class="lang-item"
-                :class="{
+        <div
+            v-for="lang in langs"
+            :key="lang.code"
+            v-on:click="changeLang(lang.code)"
+            :title="lang.code"
+            :disabled="$store.state.currentLang === lang.code"
+            class="lang-item"
+            :class="{
                     'selected-lang': $store.state.currentLang === lang.code,
                 }"
-            >
-                {{ lang.name }}
-            </div>
-        </div>
+        >{{lang.name}}</div>
     </div>
 </template>
 

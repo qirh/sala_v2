2bb1821a5f0feba724418481a22fba44234fab9e
saleh <saleh@alghusson.com>
2020-05-05 00:39:32 -0400

    new langs styling

--- src/components/LangSwitcher.vue
+++ src/components/LangSwitcher.vue
@@ -1,11 +1,14 @@
 <template>
-    <div v-if="langs.length">
+    <div v-if="langs.length" class="langs-list">
         <div v-for="lang in langs" v-bind:key="lang.code">
             <div
-                v-if="lang.code != currentLang"
                 v-on:click="changeLang(lang.code)"
                 :title="lang.code"
+                :disabled="$store.state.currentLang === lang.code"
                 class="lang-item"
+                :class="{
+                    'selected-lang': $store.state.currentLang === lang.code,
+                }"
             >
                 {{ lang.name }}
             </div>

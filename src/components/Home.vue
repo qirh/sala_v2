2daee1bc664effedb2b9320e3e8100921d32ccb9
saleh <saleh@alghusson.com>
2020-05-19 01:39:38 -0400

    grid

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,24 +1,36 @@
 <template>
-    <div>
-        <div class="home-text">
-            <p class="home-greet">{{ $t('homeGreet') }}</p>
-            <p class="home-title" v-html="$t('homeTitleOne')"></p>
-            <p class="home-title" v-html="$t('homeTitleTwo')"></p>
-            <p v-html="$t('homeSubOne')"></p>
-            <p v-html="$t('homeSubTwo')"></p>
-        </div>
-        <div class="home-settings">
-            <Corner></Corner>
-            <ThemeToggler></ThemeToggler>
+    <div class="grid">
+        <div class="grid-langs">
             <LangSwitcher
                 @updateLangFromSwitcher="updateLangFromHome"
             ></LangSwitcher>
+        </div>
+        <div class="grid-theme">
+            <ThemeToggler></ThemeToggler>
+        </div>
+        <div class="grid-icons">
+            <Corner></Corner>
+            <Icons></Icons>
+        </div>
+        <div class="grid-main">
+            <p class="main-greet">{{ $t('homeGreet') }}</p>
+            <p class="main-title" v-html="$t('homeTitleOne')"></p>
+            <p class="main-title" v-html="$t('homeTitleTwo')"></p>
+            <p v-html="$t('homeSubOne')"></p>
+            <p v-html="$t('homeSubTwo')"></p>
+        </div>
+        <div class="grid-picture">
+            <img class="picture" src="/assets/moi.jpg" />
+        </div>
+        <div class="grid-footer">
+            <p>FOOOOTER</p>
         </div>
     </div>
 </template>
 
 <script>
 import Corner from '@/components/Corner';
+import Icons from '@/components/Icons';
 import ThemeToggler from '@/components/ThemeToggler';
 import LangSwitcher from '@/components/LangSwitcher';
 
@@ -26,6 +38,7 @@ export default {
     name: 'Home',
     components: {
         Corner,
+        Icons,
         ThemeToggler,
         LangSwitcher,
     },
--- src/components/Icons.vue
+++ src/components/Icons.vue
@@ -1,43 +1,35 @@
 <template>
-    <div>
-        <div>
-            <a
-                href="https://t.me/salghusson"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <font-awesome-icon
-                    :icon="['fab', 'telegram']"
-                ></font-awesome-icon>
-            </a>
-            <a
-                href="https://github.com/qirh"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <font-awesome-icon
-                    :icon="['fab', 'github']"
-                ></font-awesome-icon>
-            </a>
-            <a
-                href="https://www.goodreads.com/sal7"
-                target="_blank"
-                rel="noopener noreferrer"
-                :title="$t('reading')"
-            >
-                <font-awesome-icon
-                    :icon="['fab', 'goodreads']"
-                ></font-awesome-icon>
-            </a>
-            <a
-                href="https://saleh.soy"
-                target="_blank"
-                rel="noopener noreferrer"
-                :title="$t('writing')"
-            >
-                <font-awesome-icon :icon="['fa', 'blog']"></font-awesome-icon>
-            </a>
-        </div>
+    <div class="icons">
+        <a
+            href="https://t.me/salghusson"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <font-awesome-icon :icon="['fab', 'telegram']"></font-awesome-icon>
+        </a>
+        <a
+            href="https://github.com/qirh"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <font-awesome-icon :icon="['fab', 'github']"></font-awesome-icon>
+        </a>
+        <a
+            href="https://www.goodreads.com/sal7"
+            target="_blank"
+            rel="noopener noreferrer"
+            :title="$t('reading')"
+        >
+            <font-awesome-icon :icon="['fab', 'goodreads']"></font-awesome-icon>
+        </a>
+        <a
+            href="https://saleh.soy"
+            target="_blank"
+            rel="noopener noreferrer"
+            :title="$t('writing')"
+        >
+            <font-awesome-icon :icon="['fa', 'blog']"></font-awesome-icon>
+        </a>
     </div>
 </template>
 

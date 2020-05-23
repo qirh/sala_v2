33aeb227bd0721d4a360312fa6ee216f00a731a1
saleh <saleh@alghusson.com>
2020-05-23 04:10:16 -0400

    imagemin

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
 module.exports = {
+    configureWebpack: {
+        plugins: [
+            new ImageminPlugin({
+                disable: process.env.NODE_ENV !== 'production',
+            }),
+        ],
+    },
     pluginOptions: {
         i18n: {
             enableInSFC: true,

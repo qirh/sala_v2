6b8d36017a0d0d876d1ea0d612d1ce7a67e238d8
saleh <saleh@alghusson.com>
2020-06-02 00:28:57 -0400

    remove console logs

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,20 +4,10 @@ import {register} from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
     register(`${process.env.BASE_URL}service-worker.js`, {
-        ready() {
-            console.log('~~successfully loaded from sw');
-        },
         updated() {
-            console.log('~~new content available, refreshing');
             setTimeout(() => {
                 window.location.reload(true);
             }, 1000);
-        },
-        offline() {
-            console.log('~~running in offline mode');
-        },
-        error(error) {
-            console.log(`~~\n${error}`);
         },
     });
 }

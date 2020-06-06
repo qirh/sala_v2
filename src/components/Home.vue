71d5f1661f26dd76370c47ec71d88d813891b3a8
saleh <saleh@alghusson.com>
2020-06-05 23:02:18 -0400

    basic css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,9 +8,11 @@
             <Icons></Icons>
         </div>
         <div id="grid-main">
-            <div class="grid-text">
+            <div class="grid-title">
                 <p class="main-title" v-html="$t('title1')"></p>
                 <p class="main-title" v-html="$t('title2')"></p>
+            </div>
+            <div class="grid-paragraphs">
                 <p v-html="$t('p1')"></p>
                 <p>
                     {{ $t('p2') }}

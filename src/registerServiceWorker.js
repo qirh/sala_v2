8aa18c82d9e2f57d8ec5d181e53aefcb672f4561
saleh <saleh@alghusson.com>
2020-06-20 10:13:55 -0400

    remove sw

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,11 +1,11 @@
-import {register} from 'register-service-worker';
+// import {register} from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
-    register(`${process.env.BASE_URL}service-worker.js`, {
-        updated() {
-            setTimeout(() => {
-                window.location.reload(true);
-            }, 1000);
-        },
-    });
+    // register(`${process.env.BASE_URL}service-worker.js`, {
+    //     updated() {
+    //         setTimeout(() => {
+    //             window.location.reload(true);
+    //         }, 1000);
+    //     },
+    // });
 }

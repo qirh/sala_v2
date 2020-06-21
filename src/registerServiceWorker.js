5378b6e78cde428c0cb5f2b79b5f938cce6a0b8b
saleh <saleh@alghusson.com>
2020-06-21 00:49:54 -0400

    remove old logs

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -4,21 +4,6 @@ import {register} from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
     register(`${process.env.BASE_URL}service-worker.js`, {
-        // ready() {
-        //     console.log(
-        //         'App is being served from cache by a service worker.\n' +
-        //             'For more details, visit https://goo.gl/AFskqB',
-        //     );
-        // },
-        // registered() {
-        //     console.log('Service worker has been registered.');
-        // },
-        // cached() {
-        //     console.log('Content has been cached for offline use.');
-        // },
-        // updatefound() {
-        //     console.log('New content is downloading.');
-        // },
         updated() {
             console.log('New content is available; please refresh.');
         },
@@ -27,8 +12,5 @@ if (process.env.NODE_ENV === 'production') {
                 'No internet connection found. App is running in offline mode.',
             );
         },
-        // error(error) {
-        //     console.error('Error during service worker registration:', error);
-        // },
     });
 }

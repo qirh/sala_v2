2640a118fe7166e9519d32a8c0cd64ce3893daea
saleh <saleh@alghusson.com>
2020-08-30 11:36:39 -0400

    about component

---
+++ src/components/About.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <a :href="gitLink">{{gitHash}}</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'About',
+    data: () => {
+        return {
+            gitHash: GIT_DESCRIBE.hash,
+            gitLink: 'https://github.com/qirh/sala_v2/commit/' + GIT_DESCRIBE.hash,
+        };
+    },
+};
+</script>

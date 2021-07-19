4289cc16a8e2d6fd52d403344fc56774f814c28f
saleh <saleh.alghusson@shopify.com>
2021-07-18 22:21:42 -0400

    lint

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <a style="position: absolute;top: 8px;left: 16px;" href="javascript:history.back()">&lt;-back</a>
-        <br>
+        <a
+            style="position: absolute;top: 8px;left: 16px;"
+            href="javascript:history.back()"
+        >
+            &lt;-back
+        </a>
         commit: <a :href="gitLink">{{ gitHash }}</a>
     </div>
 </template>

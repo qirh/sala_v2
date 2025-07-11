7c3b5a511a310cac6659f2e9a1b484f883601d1e
saleh <saleh@alghusson.com>
2025-07-11 09:31:34 -0400

    new page

---
+++ src/components/Bday25.vue
@@ -0,0 +1,86 @@
+<template>
+    <div>
+        <a
+            style="position: absolute; top: 8px; left: 16px"
+            href="javascript:history.back()"
+        >
+            &lt;-back
+        </a>
+        <br />
+        <h2>Invitation to celebrate my birthday (2025)</h2>
+        <img
+            width="200px"
+            height="auto"
+            rel="preload"
+            src="/assets/bday25_moi.jpg"
+        />
+
+        <br />
+        <br />
+        Friends, familiares, familia, welcome to another one of my (some would say stupid, i would say creative) ideas
+        <br />
+        <br />
+        As you might know, I was born around this general time of year many years ago. And I decided to participate in the festivities that people usually partake in when this time of year comes
+        <br />
+        <br />
+        So, I thought why not celebrate like them. And so ... I'm gonna celebrate by running from NJ to CT. A tri-state run
+        <br />
+        <br />
+        
+        <img
+            width="800px"
+            height="auto"
+            rel="preload"
+            src="/assets/bday25_route.png"
+        />
+        <br />
+        <br />
+        I'm not sure what the total mileage will be, but I think it should be close to ~26 miles 😨
+        <br />
+        <br />
+        The general route is Fort Lee, NJ -> Manhattan -> Then roughly follow the East Coast Greenway from The Bronx -> Westchester -> Greenwich, CT.
+        Then come back in the Metro North.
+        <br />
+        <br />
+        I'm gonna do it on Sunday, July 13th. And start at 7am from Fort Lee, NJ.
+        <br />
+        <br />
+        Now you might be wondering, how can I help you celebrate?
+        <br />
+        <br />
+        Well ... I think this will be a very miserable experience. But if you want to join me (in spirit), you can add songs to this <a
+            href="https://open.spotify.com/playlist/2WFFJGE3XG2rp29MqHM9wr?si=ee8573c051864aec&pt=5a1b2e5e37a94e77b1b3eb0885052cca"
+            >Spotify playlist.</a
+        > but please keep it to 5 songs or less, I'll be running for a day not a week haha.
+        <br />
+        <br />
+        And if you want to follow me I will post a link here on Saturday night for my live Google Maps location (since I don't have an iPhone anymore, find my friends doesn't work anymore).
+        <br />
+        <br />
+        Ultimately, you are seeing this page because you are very near and dear to my heart and I want to thank you for your beautiful presence in my life ❤️
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Bday25',
+};
+</script>
+
+<style scoped lang="scss">
+div,
+h2,
+h3,
+h4,
+p,
+a,
+b {
+    font-family: 'Roboto', Arial, sans-serif;
+    font-size: 1.1em;
+}
+img,
+.container {
+    max-width: 100%;
+    height: auto;
+}
+</style>

37fadebb1b8224f28794b7581a83ff731aa8eb98
Saleh Alghusson <saleh@perplexity.ai>
2025-10-28 09:01:07 -0400

    wip

---
+++ src/components/NYCMarathon25.vue
@@ -0,0 +1,118 @@
+<template>
+    <div>
+        <a
+            style="position: absolute; top: 8px; left: 16px"
+            href="https://saleh.sh"
+        >
+            &lt;-back
+        </a>
+        <br />
+        <h2>2nd annual Invitation to see me suffer the (2025 this time)</h2>
+
+        <br />
+        <br />
+        Hello and welcome to my NYC marathon 2025 edition fan page.
+        
+        As you (should) know, 2025 was the hardest year of my life in a while. And instead of sitting down actually solving my problems, I decided to run run run away from them.
+
+        Thankfully I ran so much that I outran them and now their in the backmirror so far away and this year has turned around and is much much better (alhamdulilah).
+        
+        Anyways I'm running the marathon on Nov 2, 2025.
+
+        <br />
+        <br />
+
+        I know it may be hard to believe for some of you, but I did put effort and trained extensively for this one. I follow a 12 week training plan:
+        <br />
+        <br />
+        <h3>Week 1</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_1.jpeg"
+        />
+
+        <h3>Week 2</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_2.jpeg"
+        />
+
+        <h3>Week 3</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_3.jpeg"
+        />
+
+        <h3>Week 4</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_4.jpeg"
+        />
+
+        <h3>Week 5</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_5.jpeg"
+        />
+
+        <h3>Week 6 -- this was pretty rough training, i worked out so much my neck hurt</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_6.jpeg"
+        />
+
+        <h3>Week 7 -- it was hard to get out of bed and train that day, but with persistance and strength, i made it</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_7.jpeg"
+        />
+
+        <h3>Weeks 7-11 -- i trained a lot but no one documented it</h3>
+        <h3>Week 12 -- ❤️❤️❤️❤️❤️❤️❤️ absolutley the best</h3>
+        <img
+            width="300px"
+            height="auto"
+            rel="preload"
+            src="/assets/training_8.jpeg"
+        />
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NYCMarathon25',
+};
+</script>
+
+<style scoped lang="scss">
+div,
+h2,
+h3,
+h4,
+p,
+a,
+b {
+    font-family: 'Roboto', Arial, sans-serif;
+    font-size: 1.1em;
+}
+img,
+.container {
+    max-width: 100%;
+    height: auto;
+}
+</style>
